d97fbdb917b04887dd5ac0a008e3e51e5421a5d2
Hareem <hareemiftikhar91@gmail.com>
2023-04-02 22:58:27 +0500

    add

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# h="hreem"
+# print(h[:])
+# # slicing
+# h={"a":12}
+# # a is a
+# h["w"]=14
+# # print(h["hareem"])
+# print(h.get("hareem"))
+# print(h.keys())
+# print(h)
+# print(h.items())
+# for i in range(5):
+#     print(i)
+#     # print(i,i*
+# marks=[1,2,3,4]
+# for item in marks:
+#     print(item)
+# while(True):
+#     a=input("who is best? ")
+#     if(a=="arfa"):
+#         print("you are right!")
+#     else:
+#         print("amazing but not best")
+# try:
+#     n=int(input("enter a number please"))
+#     print(n,"is a number,great!")
+#
+# except Exception as e:
+#     print("you nned to know what number is.. huh, \nerror occured\n",e)
+#short method
+with open("FileHandling.txt","w") as f:
+    f.write("Hareem is back to programming")
+#context  method to make a file - long method have to close
+fp=open("newFile.txt","w")
+fp.write("she is amazing")
+fp.close()
+with open("newFile.txt","r") as f:
+    s=f.read()
+    print(s)
+with open("FileHandling.txt","a") as f:
+    f.write("Hareem is doing great")
